99347537c9dbb375c74c12f26c8240054f0a0608
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-06-12 19:17:41 +0300

    Project Creation

---
+++ src/main/java/com/tsimo/spendwise/SpendwiseApplication.java
@@ -0,0 +1,13 @@
+package com.tsimo.spendwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendwiseApplication.class, args);
+	}
+
+}
